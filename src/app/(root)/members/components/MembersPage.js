fad374a2d8858e92d926debc4f8b0823401bc186
kimlong <kimlong5244@gmail.com>
2025-01-23 17:43:24 +0700

    update members

--- src/app/(root)/members/components/MembersPage.js
+++ src/app/(root)/members/components/MembersPage.js
@@ -24,7 +24,7 @@ const MembersPage = ({ initialData = [] }) => {
         description="Our leader is deeply passionate about creating positive change through charity work in Cambodia. With unwavering dedication, they guide our mission to support communities and inspire meaningful impact."
         bg="group-hover:bg-[#004aad]"
         color="text-[#004aad]"
-        hcolor="hover:text-[#004aad]"
+        hcolor="hover:bg-[#004aad]"
       />
       <Team
         teamMembers={memberOnly}
@@ -32,7 +32,7 @@ const MembersPage = ({ initialData = [] }) => {
         description="Our team is passionate about making a positive impact through charity work in Cambodia. United by our dedication, we strive to support communities and create lasting change."
         bg="group-hover:bg-[#cd2f34]"
         color="text-[#cd2f34]"
-        hcolor="hover:text-[#cd2f34]"
+        hcolor="hover:bg-[#cd2f34]"
       />
       <Team
         teamMembers={volunteerOnly}
@@ -40,7 +40,7 @@ const MembersPage = ({ initialData = [] }) => {
         description="Our volunteers are driven by compassion and a commitment to making a difference. Through their dedication, they play a vital role in supporting communities and bringing positive change to Cambodia."
         bg="group-hover:bg-[#fbb80f]"
         color="text-[#fbb80f]"
-        hcolor="hover:text-[#fbb80f]"
+        hcolor="hover:bg-[#fbb80f]"
       />
       <Team
         teamMembers={alumniOnly}
@@ -48,7 +48,7 @@ const MembersPage = ({ initialData = [] }) => {
         description="Our alumni are a testament to the lasting impact of our mission. With their continued support and dedication, they inspire and contribute to meaningful change in communities across Cambodia."
         bg="group-hover:bg-[#0097b2]"
         color="text-[#0097b2]"
-        hcolor="hover:text-[#0097b2]"
+        hcolor="hover:bg-[#0097b2]"
       />
     </div>
   );
