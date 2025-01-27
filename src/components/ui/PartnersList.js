96afeaffa039195a954776cd91258c1bc004003b
kimlong <kimlong5244@gmail.com>
2025-01-27 15:29:34 +0700

    enhance partner logo quality

--- src/components/ui/PartnersList.js
+++ src/components/ui/PartnersList.js
@@ -33,13 +33,18 @@ const PartnersList = ({ direction = "left" }) => {
           {partnerList.map((partner, index) => {
             return (
               <div key={index}>
-                <Image
+                {/* <Image
                   className="max-w-[100px] md:max-w-[120px] mr-8 md:mr-16 hover:scale-125 transition-transform rounded-lg" //80px
                   src={partner.partnerLogo}
                   alt="partner"
                   width={120} // Adjust width to ensure visibility on small screens
                   height={120}
-                />
+                /> */}
+                <div
+                  key={index}
+                  className="bg-cover bg-center mr-8 md:mr-14 w-[110px] h-[110px] md:w-[120px] md:h-[120px] rounded-lg hover:scale-125 transition-transform"
+                  style={{ backgroundImage: `url(${partner.partnerLogo})` }}
+                ></div>
               </div>
             );
           })}
