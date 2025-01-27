8a83d7cb56e6cd62ffd94ef41f5920d003402663
kimlong <kimlong5244@gmail.com>
2025-01-27 15:15:17 +0700

    enhance partner logo quality

--- src/components/ui/PartnersList.js
+++ src/components/ui/PartnersList.js
@@ -28,9 +28,7 @@ const PartnersList = ({ direction = "left" }) => {
           gradient={false}
           gradientWidth={0}
           direction={direction}
-          // className="flex justify-between items-center py-3 w-full bg-primary border-[3px] dark:border-white border-black"
-
-          className="flex justify-between items-center py-3 w-full "
+          className="flex justify-start items-center py-3 w-full overflow-hidden"
         >
           {partnerList.map((partner, index) => {
             return (
@@ -39,8 +37,8 @@ const PartnersList = ({ direction = "left" }) => {
                   className="max-w-[100px] md:max-w-[120px] mr-8 md:mr-16 hover:scale-125 transition-transform rounded-lg" //80px
                   src={partner.partnerLogo}
                   alt="partner"
-                  width={150}
-                  height={150}
+                  width={120} // Adjust width to ensure visibility on small screens
+                  height={120}
                 />
               </div>
             );
